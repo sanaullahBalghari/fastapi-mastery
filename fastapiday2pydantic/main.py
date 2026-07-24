fa4c8122a9ebbf60f9f9f19c4f3adf1740779b52
sanaullahBalghari <sanaullahakhonzada289@gmail.com>
2026-07-24 18:59:19 +0500

    Implement CRUD operations for student management in FastAPI

--- fastapiday2pydantic/main.py
+++ fastapiday2pydantic/main.py
@@ -4,12 +4,69 @@ from pydantic import BaseModel
 
 app=FastAPI()
 class Student(BaseModel):
+    id: int
     name:str
-    age:int
+    age: int
+    department: str
+
+students = []
+
 
 
 @app.post("/student")
 
 def create_student(student:Student):
-    return student
+    students.append(student)
+    return {
+        "message": "Student Added",
+        "student": student
+    }
 
+@app.get("/students")
+def get_students():
+
+    return students
+
+
+@app.get("/student/{student_id}")
+
+def get_student(student_id:int):
+    for student in students:
+        if student.id == student_id:
+            return student
+        return {"message":"student not found"}
+
+
+@app.put("/students/{student_id}")
+def update_student(student_id: int, updated_student: Student):
+
+    for index, student in enumerate(students):
+
+        if student.id == student_id:
+
+            students[index] = updated_student
+
+            return {
+                "message": "Updated Successfully",
+                "student": updated_student
+            }
+
+    return {
+        "message": "Student Not Found"
+    }
+
+
+@app.delete("/students/{student_id}")
+
+def delete_student(student_id:int):
+    for student in students:
+        if student.id == student_id:
+            students.remove(student)
+            return{
+                "message":"Student Deleted"
+            }
+        return{"message":"student not found"}
+
+
+
+   
